4542f4e16cbafb8b3d9c8dacc1f0a91593c2535b
lianhua <itemyaimydwhg@163.com>
2018-06-20 19:16:06 +0800

    ActiveStorage has_many

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
+
+  has_many_attached :images
+
   validates_presence_of :password, :password_confirmation, on: :create
   validates :email, presence: true, uniqueness: true, format: { with: /\A([-a-z0-9+._]){1,64}@([-a-z0-9]+[.])+[a-z]{2,}\z/ }
 
   has_secure_password
+
+  scope :with_eager_loaded_images, -> { eager_load(images_attachments: :blob) }
 
   class << self
     def authorize!(credentials)
